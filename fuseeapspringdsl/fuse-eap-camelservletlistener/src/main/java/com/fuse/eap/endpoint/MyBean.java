238942951bbcb0d352c25d95c9d15a498697cc07
Sundar Rajendran <sundar.moorthy@gmail.com>
2016-11-01 16:07:21 -0400

    Camel Bootup with JndiCamelServletContextListener

---
+++ fuseeapspringdsl/fuse-eap-camelservletlistener/src/main/java/com/fuse/eap/endpoint/MyBean.java
@@ -0,0 +1,44 @@
+package com.fuse.eap.endpoint;
+
+import java.math.BigInteger;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+import com.predic8.common._1.PersonType;
+import com.predic8.crm._1.CompanyAddressType;
+import com.predic8.crm._1.CustomerType;
+
+public class MyBean implements Processor{
+
+	public void process(Exchange exchange) {
+
+		String header = exchange.getIn().getHeader("operationName", String.class);
+		if ("create".equalsIgnoreCase(header)) {
+			CustomerType type = new CustomerType();
+			PersonType personType = new PersonType();
+			CompanyAddressType addressType = new CompanyAddressType();
+			type.setPerson(personType);
+			type.setAddress(addressType);
+			type.setId(Math.abs(Math.random()) + "");
+			personType.setAge(new BigInteger("33"));
+			personType.setFirstName("Sundar");
+			personType.setLastName("Munirathnam Rajendran");
+			addressType.setCity("Atlanta");
+			addressType.setCountry("USA");
+			addressType.setCompanyName("Red Hat INC");
+			exchange.getOut().setBody(type);
+			exchange.getOut().setHeader("operationName", "create");
+		}
+		if ("get".equalsIgnoreCase(header)) {
+			exchange.getOut().setBody("777");
+			exchange.getOut().setHeader("operationName", "get");
+		}
+
+		if ("getAll".equalsIgnoreCase(header)) {
+			exchange.getOut().setBody(new Object[0]);
+			exchange.getOut().setHeader("operationName", "getAll");
+		}
+	}
+
+}
---
+++ fuseeapspringdsl/fuse-eap-camelservletlistener/src/main/java/com/fuse/eap/routes/MyLifecycle.java
@@ -0,0 +1,43 @@
+package com.fuse.eap.routes;
+
+import org.apache.camel.component.servletlistener.CamelContextLifecycle;
+import org.apache.camel.component.servletlistener.ServletCamelContext;
+import org.apache.camel.impl.JndiRegistry;
+
+import com.fuse.eap.endpoint.MyBean;
+
+public class MyLifecycle implements CamelContextLifecycle<JndiRegistry> {
+
+	public void afterAddRoutes(ServletCamelContext arg0, JndiRegistry arg1) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void afterStart(ServletCamelContext arg0, JndiRegistry registry) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void afterStop(ServletCamelContext arg0, JndiRegistry registry) throws Exception {
+		
+		
+	}
+
+	public void beforeAddRoutes(ServletCamelContext arg0, JndiRegistry registry) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void beforeStart(ServletCamelContext arg0, JndiRegistry registry) throws Exception {
+		registry.bind("cxfbean", new MyBean());
+		
+	}
+
+	public void beforeStop(ServletCamelContext arg0, JndiRegistry arg1) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
