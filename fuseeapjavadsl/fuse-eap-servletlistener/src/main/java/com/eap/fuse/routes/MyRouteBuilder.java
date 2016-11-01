ff46c911ea8fc38a4eab6e579db64e9affa183cb
Sundar Rajendran <sundar.moorthy@gmail.com>
2016-11-01 16:40:33 -0400

    Adding JNDI servlet listener example

---
+++ fuseeapjavadsl/fuse-eap-servletlistener/src/main/java/com/eap/fuse/routes/MyRouteBuilder.java
@@ -0,0 +1,19 @@
+package com.eap.fuse.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.cxf.CxfEndpoint;
+
+public class MyRouteBuilder extends RouteBuilder{
+
+	@Override
+	public void configure() throws Exception {
+		CxfEndpoint cxfEndpoint = new CxfEndpoint();
+        cxfEndpoint.setAddress("http://www.predic8.com:8080/crm/CustomerService");
+        cxfEndpoint.setWsdlURL("http://www.predic8.com:8080/crm/CustomerService?wsdl");
+        cxfEndpoint.setServiceClass("com.predic8.wsdl.crm.crmservice._1.CRMServicePT");
+        cxfEndpoint.setCamelContext(getContext());
+        
+		from("timer:get?delay=4000&repeatCount=1").setHeader("operationName",constant("getAll")).to("bean:cxfbean").to(cxfEndpoint).to("log:response?showAll=true&multiline=true");
+	}
+
+}
